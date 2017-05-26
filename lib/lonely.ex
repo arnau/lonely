e87412c22ba79dcb657d67ee68dfd19b08d7afb2
Arnau Siches <asiches@gmail.com>
2017-05-26 10:24:57 +0100

    docs: Add a bit of documentation to improve the entrypoint experience in Hex
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- lib/lonely.ex
+++ lib/lonely.ex
@@ -1,5 +1,32 @@
 defmodule Lonely do
   @moduledoc """
-  Lonely.
+  Lonely. Helpers to pipe through results `{:ok, a} | {:error, e}`.
+
+  It is common to get either a value or `nil`. A way to apply a function to
+  a value letting `nil` untouched.
+
+      iex> alias Lonely.Result
+      ...> [1, 2, 3]
+      ...> |> Enum.find(fn x -> x == 2 end)
+      ...> |> Result.wrap()
+      ...> |> Result.map(fn x -> x * 10 end)
+      ...> |> Result.unwrap()
+      2
+
+  Some other times you are certain you will be receiving a result so you can
+  leave out `wrap/1` and just map over the value. Also, you might want to
+  recover from the error.
+
+      iex> import Lonely.Result
+      ...> str = ""
+      ...> Time.from_iso8601(str)
+      ...> |> map(&Time.to_erl/1)
+      ...> |> flat_map_error(fn
+      ...>   :invalid_format -> {:ok, {0, 0, 0}}
+      ...>   reason -> {:error, {reason, str}}
+      ...> end)
+      {:ok, {0, 0, 0}}
+
+  Check the `Lonely.Result` module for more examples.
   """
 end
